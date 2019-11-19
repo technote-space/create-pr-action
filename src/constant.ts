7acd19f0958c05a56b4646cc2701d4f2177db854
Technote <technote.space@gmail.com>
2019-11-19 22:11:54 +0900

    chore: tweaks

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,3 @@
-export const DEFAULT_PR_BRANCH_PREFIX = 'create-pr-action/';
-export const ACTION_NAME              = 'Create PR Action';
-export const ACTION_OWNER             = 'technote-space';
-export const ACTION_REPO              = 'create-pr-action';
+export const ACTION_NAME  = 'Create PR Action';
+export const ACTION_OWNER = 'technote-space';
+export const ACTION_REPO  = 'create-pr-action';
